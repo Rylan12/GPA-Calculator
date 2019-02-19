87d674fd1c7ee6bd0192637d66b8c4ae295f666e
Rylan Polster <rslpolster@gmail.com>
2019-02-19 15:21:43 -0500

    Remove unused css

--- pages/index.vue
+++ pages/index.vue
@@ -33,15 +33,6 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
   'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -58,9 +49,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
